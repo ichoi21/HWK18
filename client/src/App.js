5ee4db3e7f81cb971b4f8a36d1da1d1db9ef36cb
ichoi21 <ihchoi@gmail.com>
2020-09-04 20:58:17 -0700

    Added format

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import NoMatch from "./pages/NoMatch";
 import Search from "./pages/Search";
@@ -20,6 +21,7 @@ class App extends Component {
             <Route exact path="/saved" component={Saved} />
             <Route exact path="/noMatch" component={NoMatch} />
           </Switch>
+          <Footer />
         </div>
       </Router>
     );
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <footer className="sticky-footer text-center">
+        <div className="container">
+          <span className="text-muted">
+            <small>
+              Copyright &copy; 2020{" "}
+              <a href="http://github.com/ichoi21/HWK18" target="blank">
+                i21 - HWK 18
+              </a>
+            </small>
+          </span>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
